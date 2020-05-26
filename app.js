47722b822cf2674b3220ca74c9318fe3d6d1ce4d
Jbradley-C <jbradley.chapman@gmail.com>
2020-05-26 09:45:02 -0500

    finished up the project

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //Make Js Code run after page is loaded
 document.addEventListener('DOMContentLoaded', function() {
+
 	//Creating the button and Text for the button
 	let button = document.createElement('button');
 	let btnText = document.createTextNode('Add Square');
-    
-    
-    //assigning the 1st square a number
-    let num = 1;  
+
+	//assigning the 1st square a number
+	let num = 1;
 
 	//Adding Text to the Button
 	button.appendChild(btnText);
@@ -23,36 +23,38 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	//function to create squares inside "flex-box" with Id named and incremented
 	button.addEventListener('click', function() {
-		let div = document.createElement('div');       //create the squares
-		div.classList.add('square');                   //adding a class to each square
-		div.id = num;                                  //using the variable num to name the id of each square
-		div.innerHTML = num;                          //inserting the value of the id into each square
-
-
+		let div = document.createElement('div'); //create the squares
+		div.classList.add('square'); //adding a class to each square
+		div.id = num; //using the variable num to name the id of each square
+		div.innerHTML = num; //inserting the value of the id into each square
 
 		//add listener for when each square is clicked
 		div.addEventListener('click', function() {
-			squareColor(div);    // change background color when each square is clicked
+			squareColor(div); // change background color when each square is clicked
 		});
-        
-        //add listener to check if Id/number is even or odd and delete appropriate square
+
+		//add listener to check if Id/number is even or odd and delete appropriate square
 		div.addEventListener('dblclick', function() {
-			if (div.id % 2 === 0) {  //checking for even id/number withing square
-				if (div.nextSibling !== null) { //checking to see if the square to right exists
+			if (div.id % 2 === 0) {
+				//checking for even id/number withing square
+				if (div.nextSibling !== null) {
+					//checking to see if the square to right exists
 					div.parentElement.removeChild(div.nextSibling); //deleting next square to right if it exists
 				} else {
-					alert("STOP CLICKING THE DANG BUTTON!!!!!"); //alert if square does not exist
+					alert('STOP CLICKING THE DANG BUTTON!!!!!'); //alert if square does not exist
 				}
-			} else {  //This considers the only other option for even/odd
-				if (div.previousSibling !== null) {  //Checks if square to left exists
+			} else {
+				//This considers the only other option for even/odd
+				if (div.previousSibling !== null) {
+					//Checks if square to left exists
 					div.parentElement.removeChild(div.previousSibling); //deletes if square exists
 				} else {
-					alert("STOP CLICKING THE DANG BUTTON!!!!"); //alerts if square does not exist
+					alert('STOP CLICKING THE DANG BUTTON!!!!'); //alerts if square does not exist
 				}
 			}
 		});
 
-		num++; //increment number 
+		num++; //increment number
 		flexContainer.appendChild(div); //insert squares into flex-box
 	});
 
